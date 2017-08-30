23ed38ea1a23597deac0499f7555f85c0325f341
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-08-29 19:52:37 -0700

    initial commit

---
+++ recipes.js
@@ -0,0 +1,27 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent']*/
+module.exports = {
+    'RECIPE_EN_US': {
+        'beef on weck': 'A beef on weck sandwich is made with roast beef on a kummelweck roll, often with horseradish.',
+        'blt': 'A BLT sandwich is made with bacon, lettuce, and tomato.',
+        'breakfast sandwich': 'A breakfast sandwich is often made with bacon, eggs, cheese, sausage, or ham.',
+        'cheeseburger': 'A cheeseburger is made with a meat patty, usually ground beef, on a roll with cheese, often served with lettuce, tomato, pickles, bacon, onion, and condiments such as mustard, mayonnaise, ketchup, and relish.',
+        'cheese steak': 'A cheese steak is made with beef steak on a hoagie roll with melted cheese and often additional toppings such as peppers, onions, and mushrooms.',
+        'club sandwich': 'A club sandwich is made with sliced cooked poultry, fried bacon, lettuce, tomato, and mayonnaise, often on toast.',
+        'fluffernutter': 'A fluffernutter sandwich is made with peanut butter and marshmallow fluff.',
+        'french dip': 'A french dip sandwich is made with thinly sliced roast beef on a French roll or baguette, usually served au jus.',
+        'lobster roll': 'A lobster roll is made with lobster meat served on a hot dog roll, often with butter and mayonnaise.',
+        'tuna melt': 'A tuna melt is made with tuna filling served on bread with melted cheese.',
+        'monte cristo': 'A monte cristo sandwich is made with fried ham and cheese.',
+        'muffuletta': 'A muffuletta sandwich is made with a muffuletta loaf split in half and covered with layers of marinated olive salad, mortadella, salami, mozzarella, ham, and provolone.',
+        'patty melt': 'A patty melt sandwich is made with a hamburger patty topped with caramelized onions and cheese.',
+        'peanut butter and jelly': 'A peanut butter and jelly sandwich is pretty self-explanatory.',
+        'pilgrim': 'A pilgrim sandwich is made with roast turkey, cranberries or cranberry sauce, and cheddar cheese.',
+        'po boy': 'A po boy sandwich is made with roast beef or fried seafood served on a baguette.',
+        'reuben': 'A reuben sandwich is made with corned beef, swiss cheese, sauerkraut, and russian dressing, grilled between slices of rye bread.',
+        'sloppy joe': 'A sloppy joe sandwich is made with ground beef, onions, tomato saurce or ketchup, worcestershire sauce, and other seasonincs served on a hamburger bun.',
+        'cuban': 'A cuban sandwich is made with roasted pork, swiss cheese, pickles, mustard, and sometimes salami on cuban bread',
+        'grilled cheese': 'A grilled cheese sandwich is made with melted cheese grilled between slices of bread'
+    }
+};
